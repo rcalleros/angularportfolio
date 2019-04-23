82d1852ba5e77967e2478ce026b81835eb43f8a1
richard <richardcalleros@yahoo.com>
2019-04-23 11:45:34 -0500

    test updates

--- src/app/directives/cross-browser-padding.directive.spec.ts
+++ src/app/directives/cross-browser-padding.directive.spec.ts
@@ -1,8 +1,12 @@
 import { CrossBrowserPaddingDirective } from './cross-browser-padding.directive';
 
 describe('CrossBrowserPaddingDirective', () => {
+  let mockRenderService;
+  beforeEach(()=>{
+    mockRenderService = jasmine.createSpyObj(['addStyle', 'hasPadding']);
+  });
   it('should create an instance', () => {
-    const directive = new CrossBrowserPaddingDirective();
+    const directive = new CrossBrowserPaddingDirective(mockRenderService,mockRenderService);
     expect(directive).toBeTruthy();
   });
 });
